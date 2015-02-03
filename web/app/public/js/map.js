d18c9b2bfa71b296181c445e919bdcaa564455ae
Jeremy White <dudymas@gmail.com>
2015-02-02 19:27:15 -0500

    starting: app displays a point

---
+++ web/app/public/js/map.js
@@ -0,0 +1,80 @@
+/*global ol, $ */
+
+var styleCache = {};
+var styleFunction = function() {
+    var radius = 10;
+    var style = styleCache[radius];
+    if (!style) {
+        style = [new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: radius,
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 153, 0, 0.4)'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(255, 204, 0, 0.2)',
+                    width: 1
+                })
+            })
+        })];
+        styleCache[radius] = style;
+    }
+    return style;
+};
+
+var vector = new ol.layer.Vector({
+    source: new ol.source.GeoJSON({
+        extractStyles: false,
+        projection: 'EPSG:3857',
+        url: 'map.json'
+    }),
+    style: styleFunction
+});
+
+var raster = new ol.layer.Tile({
+    source: new ol.source.Stamen({
+        layer: 'toner'
+    })
+});
+
+var map = new ol.Map({
+    layers: [raster, vector],
+    target: 'map',
+    view: new ol.View({
+        center: [0, 0],
+        zoom: 2
+    })
+});
+
+var info = $('#info');
+info.tooltip({
+    animation: false,
+    trigger: 'manual'
+});
+
+var displayFeatureInfo = function(pixel) {
+    info.css({
+        left: pixel[0] + 'px',
+        top: (pixel[1] - 15) + 'px'
+    });
+    var feature = map.forEachFeatureAtPixel(pixel, function(feature) {
+        return feature;
+    });
+    if (feature) {
+        info.tooltip('hide')
+            .attr('data-original-title', feature.get('name'))
+            .tooltip('fixTitle')
+            .tooltip('show');
+    } else {
+        info.tooltip('hide');
+    }
+};
+
+$(map.getViewport()).on('mousemove', function(evt) {
+    displayFeatureInfo(map.getEventPixel(evt.originalEvent));
+});
+
+map.on('click', function(evt) {
+    displayFeatureInfo(evt.pixel);
+});
+
